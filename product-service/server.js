51be1db990d5e4dd8b0e25f1930c2d1c0100bc06
VHarshita05 <245122749018@mvsrec.edu.in>
2026-03-09 22:39:58 +0530

    Update server and frontend build

--- product-service/server.js
+++ product-service/server.js
@@ -15,6 +15,7 @@ app.use(express.json());
 const frontendPath = path.join(__dirname, "fashion-muse-render", "dist");
 
 app.use(express.static(frontendPath));
+app.use("/assets", express.static(path.join(frontendPath, "assets")));
 
 /* -----------------------------
    LOAD PRODUCTS FROM CSV
