f089a6f80e167fc7f802ac556cb12785da7e37a7
VHarshita05 <245122749018@mvsrec.edu.in>
2026-03-10 12:21:56 +0530

    Fix routing for cart page

--- cart-service/cart-delight-ui/src/App.tsx
+++ cart-service/cart-delight-ui/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+
 import Index from "./pages/Index.tsx";
 import Cart from "./pages/Cart.tsx";
 import NotFound from "./pages/NotFound.tsx";
@@ -14,14 +15,22 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Cart />} />
+
+          {/* Home Page */}
+          <Route path="/" element={<Index />} />
+
+          {/* Cart Page */}
           <Route path="/cart" element={<Cart />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+
+          {/* Catch-all route */}
           <Route path="*" element={<NotFound />} />
+
         </Routes>
       </BrowserRouter>
+
     </TooltipProvider>
   </QueryClientProvider>
 );
