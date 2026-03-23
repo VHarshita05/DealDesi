b2f7dc80c887e8882829607cc994fad100aa6d76
VHarshita05 <245122749018@mvsrec.edu.in>
2026-03-23 22:29:06 +0530

    Connected group shopping UI

--- product-service/fashion-muse-render/src/App.tsx
+++ product-service/fashion-muse-render/src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import GroupPage from "./pages/GroupPage";
 
 import Cart from "./pages/Cart";
 import Index from "./pages/Index";
