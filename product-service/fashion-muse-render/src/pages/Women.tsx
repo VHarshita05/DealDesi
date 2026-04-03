42d9b794f07628c72e9ac44b7eb2b923f0959ec3
VHarshita05 <245122749018@mvsrec.edu.in>
2026-04-03 19:52:54 +0530

    product details

--- product-service/fashion-muse-render/src/pages/Women.tsx
+++ product-service/fashion-muse-render/src/pages/Women.tsx
@@ -131,7 +131,7 @@ export default function Women() {
           <ProductCard
             key={p.id ?? i}
             product={{
-               id: p.id ?? i, 
+               id: p.id, 
               name: p.name,
               price: p.price,
               image: p.image,
